6d7be510bd1306a7b9c45ba1988f1bc3f060c331
kendallcascante <80025458+kendallcascante@users.noreply.github.com>
2021-03-04 20:11:35 -0600

    Create linearSearch.c
    Implementación de algoritmo de búsqueda secuencial en lenguaje C

---
+++ C implementation/linearSearch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+const int ARRAY_LENGHT = 500000;
+const int MAX_RANDOM = 100000;
+const int MIN_RANDOM = 1;
+
+bool linearSearch(int value, int array[]) {
+    bool found = false;
+    for(int index=0; index<ARRAY_LENGHT; index++) { 
+        if(array[index]==value) {
+            found = true;
+            break;
+        }
+    }
+    return found;
+}
+
+int main()
+{
+    int array[ARRAY_LENGHT];
+    
+    for(int index=0; index<ARRAY_LENGHT; index++) {
+        array[index] = rand() % (MAX_RANDOM-MIN_RANDOM+1) + MIN_RANDOM;
+    }
+
+    if(linearSearch(200000, array)) {
+        printf("Value was found :)");
+    } else {
+        printf("Value was not found :(");
+    }
+
+    return 0;
+}
+
+
