312b40e76a47ad946134adeb63ba8181dc0484f2
kendallcascante <80025458+kendallcascante@users.noreply.github.com>
2021-03-04 20:08:39 -0600

    Update linearSearch.cpp

--- C++ implementation/linearSearch.cpp
+++ C++ implementation/linearSearch.cpp
@@ -8,9 +8,9 @@ const int ARRAY_LENGHT = 500000;
 const int MAX_RANDOM = 100000;
 const int MIN_RANDOM = 1;
 
-bool linearSearch(int value, int array[], int lenght) {
+bool linearSearch(int value, int array[]) {
     bool found = false;
-    for(int index=0; index<lenght; index++) { 
+    for(int index=0; index<ARRAY_LENGHT; index++) { 
         if(array[index]==value) {
             found = true;
             break;
@@ -22,11 +22,11 @@ bool linearSearch(int value, int array[], int lenght) {
 int main() {
     int array[ARRAY_LENGHT];
 
-    for(int index=0; index<(sizeof array / sizeof array[0]); index++) {
+    for(int index=0; index<ARRAY_LENGHT; index++) {
         array[index] = MIN_RANDOM + rand() % (MAX_RANDOM - MIN_RANDOM + 1);
     }
 
-    if(linearSearch(200000, array, (sizeof array / sizeof array[0]))) {
+    if(linearSearch(200000, array)) {
         cout << "Value was found :)" << endl;
     } else {
         cout << "Value was not found :(" << endl;
