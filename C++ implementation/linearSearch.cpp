bc3eece66eb539a612a68169f150b021b363425a
kendallcascante <80025458+kendallcascante@users.noreply.github.com>
2021-03-09 20:01:17 -0600

    Modificación del código C++

--- C++ implementation/linearSearch.cpp
+++ C++ implementation/linearSearch.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <time.h> 
+#include <time.h>
 
 using namespace std;
 
-const int ARRAY_LENGHT = 100000;
+const long int ARRAY_LENGHT = 500000;
 const int MAX_RANDOM = 100000;
 const int MIN_RANDOM = 1;
 
 bool linearSearch(int value, int array[]) {
     bool found = false;
-    
     for(int index=0; index<ARRAY_LENGHT; index++) { 
+        cout << " " ;
         if(array[index]==value) {
             found = true;
             break;
@@ -19,25 +19,24 @@ bool linearSearch(int value, int array[]) {
     return found;
 }
 
-int main()
-{ 
-    //time_t timeI = time(NULL);
+int main() {
 
+    time_t start, end; 
+    time(&start); 
     int array[ARRAY_LENGHT];
-    
+
     for(int index=0; index<ARRAY_LENGHT; index++) {
-    	cout << "Delay" << endl;
-        array[index] = rand() % (MAX_RANDOM-MIN_RANDOM+1) + MIN_RANDOM;
+        array[index] = MIN_RANDOM + rand() % (MAX_RANDOM - MIN_RANDOM + 1);
     }
 
     if(linearSearch(200000, array)) {
-        cout << "Value was found :)" << endl;
+        cout << "\nValue was found :)" << endl;
     } else {
-        cout << "Value was not found :(" << endl;
+        cout << "\nValue was not found :(" << endl;
     }
     
-    //time_t timeF = time(NULL);
-    //cout << "Tiempo: " << timeF-timeI << " segundos";
+    time(&end); 
+    double time_taken = double(end - start); 
+    cout << "Time taken by program is : " << fixed << time_taken << "sec";
     
-    return 0;
 }
